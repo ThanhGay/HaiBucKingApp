d136ec5c8ba3ad8b0494cd8185e56b6c0a4d5c03
Phạm Quyết <mac@MacBook-Pro-cua-mac-2.local>
2024-03-29 23:08:19 +0700

    Splash

---
+++ src/app-views/Splash/Splash.tsx
@@ -0,0 +1,37 @@
+import { View, Text, ActivityIndicator, Image } from 'react-native';
+import React from 'react';
+// import {useSpring as MySpring} from 'react-spring';
+
+export default function SplashScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignContent: 'center',
+      }}
+    >
+      <Image
+        source={require('/Users/mac/Desktop/DoAnDNT/HaiBucKingApp/src/assets/images/movie-1.png')}
+      />
+      <ActivityIndicator style={{ paddingTop: 50 }} color={'green'} size={35} />
+    </View>
+    //   const {x, y, scale, rotate} = MySpring({
+    //     from: {x: 0, y: 0, scale: 0, rotate: 0},
+    //     to: {x: 0, y: 0, scale: 1, rotate: 360},
+    //     config: {duration: 1000},
+    //   });
+
+    //   return (
+    //     <View>
+    //       <Image
+    //         style={{
+    //           transform: `translate(${x}, ${y}) scale(${scale}) rotate(${rotate}deg)`,
+    //         }}
+    //         source={require('../../Images/Logo/Logo-L.png')}
+    //         alt="Your logo"
+    //       />
+    // </View>
+  );
+}
