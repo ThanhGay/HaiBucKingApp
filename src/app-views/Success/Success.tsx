c2315887dd50c182ff3f821b05afa6cc2acb341f
DucThanh <phoducthanh@gmail.com>
2024-04-02 14:18:22 +0700

    Success screen

---
+++ src/app-views/Success/Success.tsx
@@ -0,0 +1,33 @@
+import DetailTicket from '@/app-componets/DetailTicket';
+import CanBackLayout from '@/layouts/CanBackLayout';
+import colors from '@/utils/colors';
+import { View, Text, StyleSheet } from 'react-native';
+
+function Success() {
+  return (
+    <View style={{ flex: 1, backgroundColor: colors.black }}>
+      <View style={styles.header}>
+        <View style={{ width: 40, height: 40 }} />
+        <Text style={styles.title}>Success</Text>
+        <View style={{ width: 40, height: 40 }}>
+          <Text>V</Text>
+        </View>
+      </View>
+      <View style={{ flex: 1 }}>
+        <DetailTicket />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginHorizontal: 16,
+  },
+  title: { fontSize: 20, fontWeight: '700', color: colors.white },
+});
+
+export default Success;
