b35e62b56a865aea2330014e93bed3e5a99cc834
DucThanh <phoducthanh@gmail.com>
2024-05-13 23:43:14 +0700

    api Admin always have token to fetch

--- src/api/movieAdmin.ts
+++ src/api/movieAdmin.ts
@@ -1,111 +1,89 @@
 import axiosClient from '../utils/axiosClient';
 import { LOCALHOST, PORT } from '../../port';
+import { getWithToken, postWithToken, putWithToken } from '@/utils';
 const MovieAdmin_URL = `http://${LOCALHOST}:${PORT}/movie`;
 
 // see category
-export const apiGetCategory = async () => {
-    const url = `${MovieAdmin_URL}/category`
-    const data = axiosClient.post(url).then((response) => {
-        return response.data
-    }).catch((eroor) => {
-        console.log(eroor);
-        
-    })
-    return data ?? {}
-}
+export const apiGetCategory = async (args: { token: string }) => {
+  const token = args.token;
+  const url = `${MovieAdmin_URL}/category`;
+
+  return getWithToken({ url, token });
+};
 
 // add Movie
 export const apiPostMovie = async (args: {
-    movieId: string, 
-    movieName: string,
-    duration: number,
-    censorship: number, 
-    language: string, 
-    release: string, 
-    expiration: string,
-    poster: string, 
-    description: string
-    categoryId: Array<any>[]
+  token: string;
+  movieId: string;
+  movieName: string;
+  duration: number;
+  censorship: number;
+  language: string;
+  release: string;
+  expiration: string;
+  poster: string;
+  description: string;
+  categoryId: Array<any>[];
 }) => {
-    const url = `${MovieAdmin_URL}/add-movie`
-    const form = JSON.stringify({
-        Movie_Id : args.movieId,
-        Movie_Name : args.movieName,
-        Duration : args.duration,
-        Censorship : args.censorship,
-        Language : args.language,
-        Release : args.release,
-        Expiration : args.expiration,
-        Description : args.description,
-        Poster : args.poster,
+  const token = args.token;
+  const url = `${MovieAdmin_URL}/add-movie`;
+  const form = JSON.stringify({
+    Movie_Id: args.movieId,
+    Movie_Name: args.movieName,
+    Duration: args.duration,
+    Censorship: args.censorship,
+    Language: args.language,
+    Release: args.release,
+    Expiration: args.expiration,
+    Description: args.description,
+    Poster: args.poster,
 
-        Category_Id: args.categoryId
-
-
-    })
-    const data = axiosClient.post(url, form).
-    then((response) => {return response.data}).
-    catch((error) => {
-        console.log(error);
-        
-    })
-    return data ?? {}
-}
-// edit Movie 
+    Category_Id: args.categoryId,
+  });
+  return postWithToken({ url, data: form, token });
+};
+// edit Movie
 export const apiPutEditMovie = async (args: {
-    movieId: string, 
-    movieName: string,
-    duration: number,
-    censorship: number, 
-    language: string, 
-    release: string, 
-    expiration: string,
-    poster: string, 
-    description: string
-    categoryId: Array<any>[]
+  token: string;
+  movieId: string;
+  movieName: string;
+  duration: number;
+  censorship: number;
+  language: string;
+  release: string;
+  expiration: string;
+  poster: string;
+  description: string;
+  categoryId: Array<any>[];
 }) => {
-    const url = `${MovieAdmin_URL}/put-movie`
-    const form = JSON.stringify({
-        Movie_Id : args.movieId,
-        Movie_Name : args.movieName,
-        Duration : args.duration,
-        Censorship : args.censorship,
-        Language : args.language,
-        Release : args.release,
-        Expiration : args.expiration,
-        Description : args.description,
-        Poster : args.poster,
-
-        Category_Id: args.categoryId
-
-
-    })
-    const data = axiosClient.put(url, form).
-    then((response) => {return response.data}).
-    catch((error) => {
-        console.log(error);
-        
-    })
-    return data ?? {}
-}
-
+  const token = args.token;
+  const url = `${MovieAdmin_URL}/put-movie`;
+  const form = JSON.stringify({
+    Movie_Id: args.movieId,
+    Movie_Name: args.movieName,
+    Duration: args.duration,
+    Censorship: args.censorship,
+    Language: args.language,
+    Release: args.release,
+    Expiration: args.expiration,
+    Description: args.description,
+    Poster: args.poster,
+    Category_Id: args.categoryId,
+  });
+  return putWithToken({ url, data: form, token });
+};
 
 // create category
-export const apiPostAddCategory = async(args: {
-    categoryId : string,
-    categoryName : string
+export const apiPostAddCategory = async (args: {
+  token: string;
+  categoryId: string;
+  categoryName: string;
 }): Promise<{ status: string; data: any; msg: string }> => {
-    const url = `${MovieAdmin_URL}/add-category`
-    const form = JSON.stringify({
-        Category_Id: args.categoryId,
-        Category_Name: args.categoryName
-    })
-    const data = axiosClient.post(url, form).then((response) => {
-        return response.data
-    }).catch((error) => {
-        console.log(error);
-        
-    })
-    return data ?? {}
-
-}
+  const token = args.token;
+  const url = `${MovieAdmin_URL}/add-category`;
+  const form = JSON.stringify({
+    Category_Id: args.categoryId,
+    Category_Name: args.categoryName,
+  });
+  return postWithToken({ url, data: form, token });
+};
--- src/api/reportAdmin.ts
+++ src/api/reportAdmin.ts
@@ -1,50 +1,38 @@
 import axiosClient from '../utils/axiosClient';
 import { LOCALHOST, PORT } from '../../port';
-import { postWithToken } from '@/utils';
+import { getWithToken, postWithToken } from '@/utils';
 const ReportAdmin_URL = `http://${LOCALHOST}:${PORT}/report`;
 
 // Quyet test API
-// Report by time  -- Done 
+// Report by time  -- Done
 export const apiGetReportByTime = async (args: {
-    minDate: any;
-    maxDate: any;
-  })=> {
-    const url = `${ReportAdmin_URL}/report-by-time`
-    const form = JSON.stringify({
-        minDate :args.minDate,
-        maxDate : args.maxDate
-    })
-    const data = axiosClient.post(url, form).then((response) => {
-        return response.data;
-      })
-      .catch((error) => {
-        console.log('error in fetch api showtimes', error);
-      });
-      return data ?? {};
-
-  };
-
-export const apiGetReportByMovie = async() => {
-    const url = `${ReportAdmin_URL}/report-by-movie`
-    const data = axiosClient.get(url).then((response) => {
-        return response.data
-    }).catch((error) => {
-        console.log('error in fetch api showtimes', error);
-        
-    });
-    return data ?? {}
-
-}
-  
-export const apiGetReportByQuarter = async(args: {
-    year: number,
-    token: string
+  token: string;
+  minDate: any;
+  maxDate: any;
 }) => {
-    const url = `${ReportAdmin_URL}/report-by-quarter`
-    const form = JSON.stringify({
-        year: args.year
+  const token = args.token;
+  const url = `${ReportAdmin_URL}/report-by-time`;
+  const form = JSON.stringify({
+    minDate: args.minDate,
+    maxDate: args.maxDate,
+  });
+  return postWithToken({ url, data: form, token });
+};
 
-})  
-    const {token} = args
-    return postWithToken({url, data: form, token})
-}
+export const apiGetReportByMovie = async (args: { token: string }) => {
+  const token = args.token;
+  const url = `${ReportAdmin_URL}/report-by-movie`;
+  return getWithToken({ url, token });
+};
+
+export const apiGetReportByQuarter = async (args: {
+  token: string;
+  year: number;
+}) => {
+  const url = `${ReportAdmin_URL}/report-by-quarter`;
+  const form = JSON.stringify({
+    year: args.year,
+  });
+  const { token } = args;
+  return postWithToken({ url, data: form, token });
+};
