2e33cfc54062be4eea42ef9d62f80ed23ff86209
DucThanh <phoducthanh@gmail.com>
2024-05-13 23:48:43 +0700

    dispatch api detail movie - 50%

--- src/redux/features/movie/movieSlice.ts
+++ src/redux/features/movie/movieSlice.ts
@@ -17,13 +17,14 @@ export interface MovieState {
   imageDirectors: Array<string> | Array<null>;
   actors: Array<string> | null;
   imageActors: Array<string> | Array<null>;
-  movie: any
+  movie: any;
 }
 
 export const getDetailMovie = createAsyncThunk(
   'movie/getDetail',
   async (args: { movieId: string }) => {
     const res = await apiDetailMovie(args);
+    console.log(args,'dataRes1:',res);
     if (res.status) {
       const formatMovie = transformDataMovie(res.data);
       const _movie = {
@@ -36,21 +37,21 @@ export const getDetailMovie = createAsyncThunk(
 );
 
 const initialState: MovieModel = {
-    id: '',
-    name: '',
-    duration: 0,
-    censorship: 0,
-    language: '',
-    release: '',
-    expiration: '',
-    description: '',
-    poster: '',
-    categories: '',
-    directors: [],
-    imageDirectors: [],
-    actors: [],
-    imageActors: [],
-    // movie: null
+  id: '',
+  name: '',
+  duration: 0,
+  censorship: 0,
+  language: '',
+  release: '',
+  expiration: '',
+  description: '',
+  poster: '',
+  categories: '',
+  directors: [],
+  imageDirectors: [],
+  actors: [],
+  imageActors: [],
+  // movie: null
 };
 
 export const movieSlice = createSlice({
@@ -58,26 +59,33 @@ export const movieSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(
-      getDetailMovie.fulfilled,
-      (state, action: PayloadAction<any>) => {
-        console.log('success');
-        
-        state.id = action.payload.Movie_Id;
-        state.name = action.payload.Movie_Name;
-        state.duration = action.payload.Duration;
-        state.censorship = action.payload.Censorship;
-        state.language = action.payload.Language;
-        state.release = action.payload.Release;
-        state.description = action.payload.Description;
-        state.poster = action.payload.Poster;
-        state.categories = action.payload.Categories;
-        state.directors = action.payload.Director;
-        state.imageDirectors = action.payload.imageDirector;
-        state.actors = action.payload.Actor;
-        state.imageActors = action.payload.imageActor;
-      },
-    );
+    builder
+      .addCase(getDetailMovie.pending, () => {
+        console.log('pending');
+      })
+      .addCase(
+        getDetailMovie.fulfilled,
+        (state, action: PayloadAction<any>) => {
+          console.log('success', action.payload);
+
+          state.id = action.payload.Movie_Id;
+          state.name = action.payload.Movie_Name;
+          state.duration = action.payload.Duration;
+          state.censorship = action.payload.Censorship;
+          state.language = action.payload.Language;
+          state.release = action.payload.Release;
+          state.description = action.payload.Description;
+          state.poster = action.payload.Poster;
+          state.categories = action.payload.Categories;
+          state.directors = action.payload.Director;
+          state.imageDirectors = action.payload.imageDirector;
+          state.actors = action.payload.Actor;
+          state.imageActors = action.payload.imageActor;
+        },
+      )
+      .addCase(getDetailMovie.rejected, () => {
+        console.log('rejected');
+      });
   },
 });
 
